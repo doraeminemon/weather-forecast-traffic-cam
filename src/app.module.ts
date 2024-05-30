1cdc016a73c637cfb9d2bc1b5664552a8134a0ec
Tyson Do <doraeminemon@gmail.com>
2024-05-30 13:17:27 +0700

    Deploy on Fly.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     HttpModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'weather-forecast-traffic-cam',
-      entities: [__dirname + '/../**/*.entity.js'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      process.env.NODE_ENV === 'production'
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+          }
+        : {
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'weather-forecast-traffic-cam',
+            entities: [__dirname + '/../**/*.entity.js'],
+            synchronize: true,
+          },
+    ),
     LocationsModule,
   ],
   controllers: [AppController],
